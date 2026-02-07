025968ccc6ed9d534d6412dd1c000b0c19905e67
Voltamon <deswarnavo@gmail.com>
2026-02-07 23:02:51 +0530

    Fixed Config

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,5 +3,9 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../server/assets',
+    emptyOutDir: true,
+  },
   plugins: [preact()],
 });
